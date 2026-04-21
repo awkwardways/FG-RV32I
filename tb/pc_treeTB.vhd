1dc1b6ba17d485629adcd3ccc60bef806742b602
Franco <franco.matteodadavila@gmail.com>
2026-04-20 22:19:14 -0600

    Added pipeline register. Does not work.

--- tb/pc_treeTB.vhd
+++ tb/pc_treeTB.vhd
@@ -15,17 +15,21 @@ architecture sim of pc_treeTB is
   signal address_tb     : std_logic_vector(ADDR_WIDTH_TB - 1 downto 0) := x"ffaa0127";
   signal offset_tb      : std_logic_vector(ADDR_WIDTH_TB - 1 downto 0) := x"aa000033";
   signal mar_addr_out   : std_logic_vector(ADDR_WIDTH_TB - 1 downto 0);
-  signal ram_dout_tb    : std_logic_vector(DATA_WIDTH_tb - 1 downto 0);
+  signal ram_dout_tb    : std_logic_vector(DATA_WIDTH_TB - 1 downto 0);
   signal stall_tb       : std_logic;
   signal address_src_tb : std_logic := '0';
   signal pc_mod_tb      : std_logic := '0';
   signal wre_tb         : std_logic;
   signal reset_tb       : std_logic;
   signal clk_tb         : std_logic := '0';
+  signal inst_out_tb    : std_logic_vector(DATA_WIDTH_TB - 1 downto 0);
+  signal pc_out_tb      : std_logic_vector(DATA_WIDTH_TB - 1 downto 0);
+  signal pipe_reg_wre_tb: std_logic;
 
 begin
 
   clk_tb <= not clk_tb after CLK_PERIOD / 2;
+  pipe_reg_wre_tb <= wre_tb;
   
   MAR: entity work.memory_address_register(rtl)
   generic map(
@@ -69,10 +73,25 @@ begin
     clk => clk_tb
   );
 
+  pipeline_reg: entity work.idif_register(rtl)
+  generic map(
+    ADDR_WIDTH => ADDR_WIDTH_TB
+  )
+  port map(
+    wre             => pipe_reg_wre_tb,
+    reset           => reset_tb,
+    clk             => clk_tb,
+    pc_in           => mar_addr_out,
+    pc_out          => pc_out_tb,
+    instruction_in  => ram_dout_tb,
+    instruction_out => inst_out_tb
+  );
+
   stimuli: process
   begin
-    reset_tb <= '0';
+    reset_tb <= '1';
     wait until rising_edge(clk_tb);
+    reset_tb <= '0';
     assert mar_addr_out = x"00000000" report "Address being output by MAR does not match expected (0x00000000)" severity failure;
     wait until falling_edge(clk_tb);
     wre_tb <= '1';
