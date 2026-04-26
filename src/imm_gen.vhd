5662c7fdb4b3f9f2090ea9912556415f0fc24b31
Franco <franco.matteodadavila@gmail.com>
2026-04-26 12:05:58 -0600

    Added ifex pipeline register. Added register file to idif testbench, immediate generator and ifex pipeline register. Pipeline works up until decode. Timing on execution fase is kind of iffy

--- src/imm_gen.vhd
+++ src/imm_gen.vhd
@@ -9,7 +9,8 @@ generic(
 );
 port(
   immediate   : out std_logic_vector(IMM_WIDTH - 1 downto 0);
-  instruction : in std_logic_vector(INST_WIDTH - 1 downto 0)
+  instruction : in std_logic_vector(INST_WIDTH - 1 downto 0);
+  imm_found   : out std_logic;
 );
 end entity immediate_generator;
 
@@ -24,20 +25,24 @@ begin
       when "0010011" | "1100111" | "0000011" => 
         immediate(11 downto 0) <= instruction(31 downto 20);
         immediate(31 downto 12) <= (others => '0') when funct3 = "011" else (others => instruction(31));
+        imm_found <= '1';
       
       when "0110111" | "0010111" => 
         immediate(19 downto 0) <= instruction(31 downto 12);
         immediate(31 downto 20) <= (others => instruction(31));
+        imm_found <= '1';
 
       when "1100011" => 
         immediate(11 downto 0) <= instruction(31) & instruction(7) & instruction(30 downto 25) & instruction(11 downto 8);
         immediate(31 downto 12) <= (others => '0') when funct3 = "110" or funct3 = "111" else (others => instruction(31));
+        imm_found <= '1';
 
       when "0100011" => 
         immediate(11 downto 0) <= instruction(31 downto 25) & instruction(11 downto 7);
         immediate(31 downto 12) <= (others => instruction(31));
+        imm_found <= '1';
 
-      when others => immediate <= (others => '0');
+      when others => immediate <= (others => '0'); imm_found <= '0';
     end case;
   end process;
 
---
+++ tb/IFIDTB.vhd
@@ -0,0 +1,182 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity IFIDTB is
+end entity IFIDTB;
+
+architecture sim of IFIDTB is
+  constant INSTR_WIDTH_TB  : integer := 32;
+  constant ADDR_WIDTH_TB   : integer := 32;
+  constant DATA_WIDTH_TB   : integer := 32;
+  constant OPCODE_WIDTH_TB : integer := 7;
+  constant IMM_WIDTH_TB    : integer := 32;
+  constant INST_WIDTH_TB   : integer := 32;
+  constant CLK_FREQ        : integer := 20e6;
+  constant CLK_PERIOD      : time    := 1000 ms / CLK_FREQ;
+
+  signal address_out_tb : std_logic_vector(ADDR_WIDTH_TB - 1 downto 0);
+  signal address_tb     : std_logic_vector(ADDR_WIDTH_TB - 1 downto 0) := x"ffaa0127";
+  signal offset_tb      : std_logic_vector(ADDR_WIDTH_TB - 1 downto 0) := x"aa000033";
+  signal mar_addr_out   : std_logic_vector(ADDR_WIDTH_TB - 1 downto 0);
+  signal ram_dout_tb    : std_logic_vector(DATA_WIDTH_TB - 1 downto 0);
+  signal stall_tb       : std_logic;
+  signal address_src_tb : std_logic := '0';
+  signal pc_mod_tb      : std_logic := '0';
+  signal wre_tb         : std_logic := '0';
+  signal reset_tb       : std_logic;
+  signal clk_tb         : std_logic := '0';
+  signal inst_out_tb    : std_logic_vector(DATA_WIDTH_TB - 1 downto 0);
+  signal pc_out_tb      : std_logic_vector(DATA_WIDTH_TB - 1 downto 0);
+  signal mem_en_tb      : std_logic;
+  signal begin_stb_tb   : std_logic;
+  signal wre_idif_tb    : std_logic;
+  signal imm_tb         : std_logic_vector(IMM_WIDTH_TB - 1 downto 0);
+  signal rs1_tb         : std_logic_vector(DATA_WIDTH_TB - 1 downto 0);
+  signal rs2_tb         : std_logic_vector(DATA_WIDTH_TB - 1 downto 0);
+  signal rd_tb          : std_logic_vector(DATA_WIDTH_TB - 1 downto 0);
+  signal funct3_out_tb  : std_logic_vector(2 downto 0);
+  signal rs1_out_tb     : std_logic_vector(DATA_WIDTH_TB - 1 downto 0);
+  signal rs2_out_tb     : std_logic_vector(DATA_WIDTH_TB - 1 downto 0);
+  signal rd_out_tb      : std_logic_vector(4 downto 0);
+  signal imm_out_tb     : std_logic_vector(DATA_WIDTH_TB - 1 downto 0);
+  signal alu_mod_out_tb : std_logic;
+
+begin
+
+  clk_tb <= not clk_tb after CLK_PERIOD / 2;
+  
+  MAR: entity work.memory_address_register(rtl)
+  generic map(
+    ADDR_WIDTH => ADDR_WIDTH_TB
+  )
+  port map(
+    address_in  => address_out_tb,
+    address_out => mar_addr_out, 
+    reset       => reset_tb,
+    clk         => clk_tb,
+    wre         => wre_idif_tb
+  );
+
+  PC_MUX_TREE: entity work.pc_tree(rtl)
+  generic map(
+    INSTR_WIDTH => INSTR_WIDTH_TB,
+    ADDR_WIDTH => ADDR_WIDTH_TB
+  )
+  port map(
+    address_out => address_out_tb,
+    stall => stall_tb,
+    pc => mar_addr_out,
+    address => address_tb,
+    offset => offset_tb,
+    address_src => address_src_tb,
+    pc_mod => pc_mod_tb
+  );
+
+  RAM: entity work.ram(rtl)
+  generic map( 
+    ADDR_WIDTH => 12,
+    DATA_WIDTH => DATA_WIDTH_TB
+  )
+  port map(
+    address => mar_addr_out(13 downto 2),
+    din => (others => '0'),
+    dout => ram_dout_tb,
+    mask => mar_addr_out(1 downto 0),
+    en => mem_en_tb,
+    wre => '0',
+    clk => clk_tb
+  );
+
+  MCU: entity work.memory_control_unit(rtl)
+  generic map(
+    ADDR_WIDTH => ADDR_WIDTH_TB,
+    DATA_WIDTH => DATA_WIDTH_TB
+  )
+  port map(
+    cpu_data_in => (others => '0'),
+    cpu_data_out => open,
+    mem_data_in => ram_dout_tb,
+    mem_data_out => open,
+    mem_en => mem_en_tb,
+    begin_stb => begin_stb_tb,
+    wre_idif => wre_idif_tb,
+    clk => clk_tb,
+    reset => reset_tb
+  );
+
+  IDIF: entity work.idif_register(rtl)
+  generic map(
+    ADDR_WIDTH => ADDR_WIDTH_TB
+  )
+  port map(
+    wre             => wre_idif_tb,
+    reset           => reset_tb,
+    clk             => clk_tb,
+    pc_in           => mar_addr_out,
+    pc_out          => pc_out_tb,
+    instruction_in  => ram_dout_tb,
+    instruction_out => inst_out_tb
+  );
+
+  REGISTERS: entity work.registers_unit(rtl)
+  generic map(
+    REG_WIDTH => DATA_WIDTH_TB
+  )
+  port map(
+    clk => clk_tb,
+    reset => reset_tb,
+    rs1_en => '1',
+    rs2_en => inst_out_tb(5) and (not inst_out_tb(2)),
+    rs1_sel => inst_out_tb(19 downto 15),
+    rs2_sel => inst_out_tb(24 downto 20),
+    rd_sel => inst_out_tb(11 downto 7), 
+    rs1 => rs1_tb,
+    rs2 => rs2_tb,
+    rd => rd_tb,
+    wre => '0'
+  );
+
+  UUT: entity work.immediate_generator(rtl)
+  generic map(
+    OPCODE_WIDTH => OPCODE_WIDTH_TB,
+    IMM_WIDTH => IMM_WIDTH_TB,
+    INST_WIDTH => INST_WIDTH_TB
+  )
+  port map(
+    immediate => imm_tb,
+    instruction => inst_out_tb
+  );
+
+  IFEX: entity work.ifex_register(rtl)
+  generic map(
+    DATA_WIDTH => DATA_WIDTH_TB
+  )
+  port map(
+    clk => clk_tb,
+    reset => reset_tb,
+    wre => wre_idif_tb,
+    funct3_in => inst_out_tb(14 downto 12),
+    funct3_out => funct3_out_tb,
+    rs1_in => rs1_tb,
+    rs1_out => rs1_out_tb,
+    rs2_in => rs2_tb,
+    rs2_out => rs2_out_tb,
+    rd_in => inst_out_tb(11 downto 7),
+    rd_out => rd_out_tb,
+    imm_in => imm_tb,
+    imm_out => imm_out_tb,
+    alu_mod_in => inst_out_tb(30),
+    alu_mod_out => alu_mod_out_tb
+  );
+
+  stimuli: process
+  begin
+    reset_tb <= '1';
+    wait until rising_edge(clk_tb);
+    reset_tb <= '0';
+    begin_stb_tb <= '1';
+    wait;
+
+  end process;
+
+end architecture sim;
